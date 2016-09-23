64d126b217be3fb3c5d54766f5af8fa1c3d78755
Jordan Bas <madjojo70@live.fr>
2016-09-23 14:24:22 +0200

    Add SFML

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include "memory/Container.hpp"
+
 
 int main(void)
 {
     std::cout << "Dead Pixel Society" << std::endl;
+
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+    while (window.isOpen())
+    {
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // évènement "fermeture demandée" : on ferme la fenêtre
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     return 0;
 }
