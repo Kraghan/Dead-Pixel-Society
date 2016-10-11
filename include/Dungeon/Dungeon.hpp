8b19ba005ffc493300da5f92b5ec623449a372d9
Aredhele <vincentstehly@hotmail.fr>
2016-10-11 15:29:59 -0400

    Data structure for map file

---
+++ include/Dungeon/Dungeon.hpp
@@ -0,0 +1,17 @@
+/*!
+ * \brief    File that contains the dungeon header
+ * \file     Dungeon.hpp
+ * \author   Aredhele
+ * \version  0.1
+ * \date     22/09/2016
+ */
+
+#ifndef __DUNGEON_HPP
+#define __DUNGEON_HPP
+
+class Dungeon
+{
+
+};
+
+#endif // __DUNGEON_HPP
